5cb932e0f6adb74fedc4a823617a5941cba34d6d
gomiero1 <marc.gomiero@gmail.com>
2024-05-18 11:51:21 +0200

    first running version

--- FannyWeb/src/main/java/com/platformteam/TestServlet.java
+++ FannyWeb/src/main/java/com/platformteam/TestServlet.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletContext;
+
 
 @WebServlet("/testme")
 public class TestServlet extends HttpServlet {
@@ -20,5 +22,11 @@ public class TestServlet extends HttpServlet {
         resp.getWriter().write("     ==\"==\n");
         resp.getWriter().write("FANNY EAR Edition - A PT Project\n");
         resp.getWriter().write("Java Version: " + System.getProperty("java.version") + "\n");
+
+        // Ottieni informazioni sull'application server
+        ServletContext context = getServletContext();
+        String serverInfo = context.getServerInfo();
+        resp.getWriter().write("Application Server: " + serverInfo + "\n");
+
     }
 }
