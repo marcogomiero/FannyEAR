add4eb5ec75aafb25fdf360129f7684ad0d892b3
gomiero1 <marc.gomiero@gmail.com>
2024-05-18 11:37:24 +0200

    first running version

---
+++ FannyWeb/src/main/java/com/platformteam/TestServlet.java
@@ -0,0 +1,26 @@
+package com.platformteam;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/testme")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Hello from EJB!\n\n");
+
+        // Stampa il banner
+        resp.getWriter().write("      ,_,   \n");
+        resp.getWriter().write("     {O,o}  \n");
+        resp.getWriter().write("     /)__)  \n");
+        resp.getWriter().write("     ==\"==\n");
+        resp.getWriter().write("FANNY EAR Edition - A PT Project\n");
+        resp.getWriter().write("Java Version: " + System.getProperty("java.version") + "\n");
+    }
+}
