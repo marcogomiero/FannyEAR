4cd05ee5e7bf750c25c90b3899a9601b1f8cf213
gomiero1 <marc.gomiero@gmail.com>
2024-05-22 10:18:14 +0200

    introduced spring boot

--- FannyWeb/src/main/java/com/platformteam/FannyServlet.java
+++ FannyWeb/src/main/java/com/platformteam/FannyServlet.java
@@ -6,6 +6,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @WebServlet("/testme")
 public class FannyServlet extends HttpServlet {
@@ -13,7 +16,6 @@ public class FannyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write("Hello from EJB!\n\n");
         resp.getWriter().write("      ,_,   \n");
         resp.getWriter().write("     {O,o}  \n");
         resp.getWriter().write("     /)__)  \n");
@@ -24,5 +26,15 @@ public class FannyServlet extends HttpServlet {
         ServletContext context = getServletContext();
         String serverInfo = context.getServerInfo();
         resp.getWriter().write("Application Server: " + serverInfo + "\n");
+        resp.getWriter().write("SpringBoot Version: " + org.springframework.boot.SpringBootVersion.getVersion());
+    }
+
+    @Scheduled(fixedRate = 180000)
+    public void scheduledCallTestme() {
+        WebClient webClient = WebClient.create("http://localhost:8080");
+        Mono<String> response = webClient.get()
+                .uri("http://localhost:8080/testme")
+                .retrieve()
+                .bodyToMono(String.class);
     }
 }
--- src/main/java/com/platformteam/NamespaceService.java
+++
@@ -1,9 +0,0 @@
-package com.platformteam;
-
-public class NamespaceService {
-
-    public String getCurrentNamespace() {
-        // Restituisce un namespace fittizio per scopi di esempio
-        return "default";
-    }
-}
